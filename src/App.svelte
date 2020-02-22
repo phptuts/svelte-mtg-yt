16c0b8768c3d9892cd5a2ed28c466015501e665d
Noah Glaser <glaserpower@gmail.com>
2020-02-21 22:09:58 -0800

    Got Components Working

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Player from "./Player.svelte";
+</script>
+
+<style>
+  main {
+    width: 80%;
+    padding: 20px;
+    border: solid gray 1px;
+    margin: 0 auto;
+    background-color: wheat;
+    margin: 10vh auto;
+  }
+  #controls-container {
+    display: flex;
+  }
+  button {
+    display: block;
+    width: 100%;
+    margin-top: 20px;
+    border: solid salmon 1px;
+    background-color: sandybrown;
+    color: rgb(61, 56, 56);
+    font-size: 20px;
+    border-radius: 3px;
+  }
+</style>
+
+<main>
+  <h1>Magic The Gathering Counter</h1>
+  <div id="controls-container">
+    <Player />
+    <Player />
+  </div>
+  <button>Start Game</button>
+</main>
---
+++ src/Player.svelte
@@ -0,0 +1,26 @@
+<style>
+  .player {
+    flex-grow: 1;
+  }
+  .plus {
+    background-color: seagreen;
+  }
+  .minus {
+    background-color: brown;
+  }
+  button {
+    font-size: 20px;
+    border-radius: 3px;
+    width: 40px;
+    color: white;
+    font-family: monospace;
+    font-weight: bold;
+  }
+</style>
+
+<div class="player">
+  <h2>20</h2>
+  <button class="plus">+</button>
+  <button class="minus">-</button>
+  <h2>Blue Wins</h2>
+</div>
